3cb831acbe10b28e944c18a1e7d61c4cc8a63996
David Rimbault <rimbault.david@gmail.com>
2014-12-14 17:32:10 +0100

    Init

---
+++ config/deploy/config.rb
@@ -0,0 +1,5 @@
+set :application, 'my_app_name'
+set :repo_url, ''
+
+#Set the type of project you're deploying : sf1, sf2
+set :project_type, 'sf2'
---
+++ config/deploy/projects/tasks/sf2_tasks.rb
@@ -0,0 +1,6 @@
+namespace :sf2 do
+    desc 'Clear cache for Symfony1 projects'
+    task :clear_cache do
+
+    end
+end
